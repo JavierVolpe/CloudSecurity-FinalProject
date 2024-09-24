e03470330d6e5c0c19c57f74b6299cf577d5445e
Javier Volpe <143789444+JavierVolpe@users.noreply.github.com>
2024-09-24 20:13:27 +0200

    Update cloud_sikkerhed_project.ps1
    Tested web server VM creation and configuration.

--- cloud_sikkerhed_project.ps1
+++ cloud_sikkerhed_project.ps1
@@ -5,12 +5,12 @@ Connect-AzAccount
 
 # Variables
 $resourceGroupName = "MyResourceGroup"
-$location = "westeurope"
+$location = "UKSouth" # westeurope
 $vmSize = "Standard_D2s_v3"
 $adminUsername = "azureuser"
 $adminPassword = ConvertTo-SecureString "A987dministrator." -AsPlainText -Force
 $adminCredential = New-Object System.Management.Automation.PSCredential($adminUsername, $adminPassword)
-$subscriptionId = (Get-AzContext).Subscription.Id
+# $subscriptionId = (Get-AzContext).Subscription.Id
 
 # Create Resource Group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -41,8 +41,9 @@ $lbRule = New-AzLoadBalancerRuleConfig -Name "MyHTTPRule" `
     -BackendPort 80
 
 # Update Load Balancer with the Rule
-$loadBalancer | Add-AzLoadBalancerRuleConfig -LoadBalancingRule $lbRule
+$loadBalancer | Add-AzLoadBalancerRuleConfig -Name "MyHTTPRule" -FrontendIpConfiguration $frontendIpConfig -BackendAddressPool $backendPool -Probe $probe -Protocol Tcp -FrontendPort 80 -BackendPort 80
 $loadBalancer | Set-AzLoadBalancer
+
 # Create Network Security Group
 $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name "MyNSG"
 
@@ -52,13 +53,14 @@ $subnet = Add-AzVirtualNetworkSubnetConfig -Name "MySubnet" -AddressPrefix "10.0
 $vnet | Set-AzVirtualNetwork
 
 # Create NICs for VMs
-$subnetId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/MyVNet/subnets/MySubnet"
+#$subnetId = (Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name "MyVNet").Subnets[0].Id
 
-#$nic1 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic1" -SubnetId $subnetId -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
-#$nic2 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic2" -SubnetId $subnetId -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
-$nic1 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic1" -SubnetId $subnet.Id -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
-$nic2 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic2" -SubnetId $subnet.Id -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
+# Get Subnet ID
+$subnetId = (Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name "MyVNet").Subnets[0].Id
 
+
+$nic1 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic1" -SubnetId $subnetId -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
+$nic2 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "MyNic2" -SubnetId $subnetId -NetworkSecurityGroupId $nsg.Id -LoadBalancerBackendAddressPoolId $backendPool.Id
 
 # Create Linux VMs with Apache2, PHP, MySQL connector
 <# $vmConfig = @{
@@ -77,95 +79,51 @@ $nic2 = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $
 $vm2 = New-AzVM @vmConfig -Name "MyVM2" -NetworkInterfaceId $nic2.Id -Image "Canonical:UbuntuServer:24.04-LTS:latest"
  #>
 
+ # Test: Create storage account to enable boot diagnosis
+
+ #$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name "jvmystorageaccount" -Location $location -SkuName "Standard_LRS" -Kind "StorageV2" -AllowBlobPublicAccess $false
+ #$existingStorageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName | Where-Object { $_.AllowBlobPublicAccess -eq $false } | Select-Object -First 1
+
  $vmConfig1 = New-AzVMConfig -VMName "MyVM1" -VMSize $vmSize | `
  Set-AzVMOperatingSystem -Linux -ComputerName "MyVM1" -Credential $adminCredential | `
- Set-AzVMSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "18.04-LTS" -Version "latest" | `
- Add-AzVMNetworkInterface -Id $nic1.Id
+ Set-AzVMSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
+ Add-AzVMNetworkInterface -Id $nic1.Id | `
+ Set-AzVMBootDiagnostic -Disable
 
 $vmConfig2 = New-AzVMConfig -VMName "MyVM2" -VMSize $vmSize | `
  Set-AzVMOperatingSystem -Linux -ComputerName "MyVM2" -Credential $adminCredential | `
- Set-AzVMSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "18.04-LTS" -Version "latest" | `
- Add-AzVMNetworkInterface -Id $nic2.Id
+ Set-AzVMSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
+ Add-AzVMNetworkInterface -Id $nic2.Id | `
+ Set-AzVMBootDiagnostic -Disable
 
 New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig1
+Start-Sleep -Seconds 5 # Wait for the first VM to be created before creating the second one
 New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig2
 
 
 
 
-# Install Apache2, PHP, MySQL connector on VMs and create index.php
+# Install Apache2, PHP, MySQL connector on VMs and get index.php from another storage account
 $script = @"
 sudo apt-get update
 sudo apt-get install -y apache2 php libapache2-mod-php php-mysql
-echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Database Connection</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-        .success {
-            color: green;
-        }
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <?php
-        \$servername = "localhost";
-        \$username = "root";
-        \$password = "A987dministrator.";
-        \$dbname = "your_database"; // Replace with your database name
-
-        // Create connection
-        \$conn = new mysqli(\$servername, \$username, \$password, \$dbname);
-
-        // Check connection
-        if (\$conn->connect_error) {
-            echo "<p class='error'>Connection failed: " . \$conn->connect_error . "</p>";
-        } else {
-            echo "<p class='success'>Connected successfully</p>";
-            echo "<p>Server: " . \$servername . "</p>";
-            echo "<p>Hostname: " . gethostname() . "</p>";
-
-            // Get all IP addresses
-            \$ips = gethostbynamel(gethostname());
-            if (\$ips !== false) {
-                echo "<p>IP Addresses:</p><ul>";
-                foreach (\$ips as \$ip) {
-                    echo "<li>\$ip</li>";
-                }
-                echo "</ul>";
-            } else {
-                echo "<p class='error'>Could not retrieve IP addresses.</p>";
-            }
-        }
-        ?>
-    </div>
-</body>
-</html>' > /var/www/html/index.php
+sudo wget -O /var/www/html/index.php https://javierstorage33.blob.core.windows.net/phpfile/index.php
+sudo rm /var/www/html/index.html
 "@
+
 Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -VMName "MyVM1" -CommandId "RunShellScript" -ScriptString $script
 Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -VMName "MyVM2" -CommandId "RunShellScript" -ScriptString $script
 
+# Open port 80
+$nsg | Add-AzNetworkSecurityRuleConfig -NetworkSecurityRule $nsgRule | Set-AzNetworkSecurityGroup
+# Update the NSG with the new rule
+$nsg | Add-AzNetworkSecurityRuleConfig -Name "Allow-HTTP" -Protocol "Tcp" -Direction "Inbound" -Priority 100 -SourceAddressPrefix "*" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange 80 -Access "Allow" | Set-AzNetworkSecurityGroup
+
+
+##################### OK UNTIL HERE #####################
+
+
+######## MYSQL Part
 # Create Application Load Balancer for MySQL VMs
 $appLbFrontendIpConfig = New-AzLoadBalancerFrontendIpConfig -Name "AppFrontEnd" -PublicIpAddress $publicIp
 $appLbBackendPool = New-AzLoadBalancerBackendAddressPoolConfig -Name "AppBackEndPool"
@@ -183,6 +141,7 @@ $mysqlVm1 = New-AzVM @vmConfig -Name "MySQLVM1" -NetworkInterfaceName $nic3.Name
 $mysqlVm2 = New-AzVM @vmConfig -Name "MySQLVM2" -NetworkInterfaceName $nic4.Name -Image "Canonical:UbuntuServer:24.04-LTS:latest"
 
 # Install MySQL on MySQL VMs
+##### TODO: Change MySQL Password > create DB > dummy data
 $mysqlScript = @"
 sudo apt-get update
 sudo apt-get install -y mysql-server
@@ -190,7 +149,7 @@ sudo apt-get install -y mysql-server
 Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -VMName "MySQLVM1" -CommandId "RunShellScript" -ScriptString $mysqlScript
 Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -VMName "MySQLVM2" -CommandId "RunShellScript" -ScriptString $mysqlScript
 
-# Create Storage Account
+# Create Storage Account for replication >> TODO: use mysql / cosmo?
 $storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name "mystorageaccount" -Location $location -SkuName "Standard_LRS"
 
 # Set up database replication (simplified example)
